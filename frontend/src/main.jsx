ae98ae5b42dfbe4278766006ad4d0bbcaa5530cf
Harshit Verma <harshit63548@gmail.com>
2024-07-02 20:58:26 +0530

    all added

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import { UserContextProvider } from './context/UserContext.jsx'
+import { CourseContextProvider } from './context/CourseContext.jsx'
+
+
+export const server='http://localhost:5000'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <UserContextProvider>
+      <CourseContextProvider>
     <App />
+    </CourseContextProvider>
+    </UserContextProvider>
+
   </React.StrictMode>,
 )
+
+//why user context present inside user context becoz there may me solmething used in course context where we need to be authenticated
--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -4,18 +4,125 @@ import jwt from "jsonwebtoken";
 import sendMail from "../middlewares/sendMail.js";
 import TryCatch from "../middlewares/TryCatch.js";
 
-// export const register=async(req,res)=>{
-//     try{
-//         res.send("regiter api");
+// export const register = TryCatch(async (req, res) => {
+//   const { email, name, password } = req.body;
+
+//   let user = await User.findOne({ email });
+
+//   if (user)
+//     return res.status(400).json({
+//       message: "User Already exists",
+//     });
+
+//   const hashPassword = await bcrypt.hash(password, 10);
+
+//   user = {
+//     name,
+//     email,
+//     password: hashPassword,
+//   };
+
+//   const otp = Math.floor(Math.random() * 1000000);
+
+//   const activationToken = jwt.sign(
+//     {
+//       user,
+//       otp,
+//     },
+//     process.env.Activation_Secret,
+//     {
+//       expiresIn: "5m",
 //     }
-//     catch(error){
-//         res.status(500).json({
-//             message:error.message,
-//         })
-//     }
-// }
+//   );
+
+//   const data = {
+//     name,
+//     otp,
+//   };
+
+//   await sendMail(email, "next-edu", data);
+
+//   res.status(200).json({
+//     message: "Otp send to your mail",
+//     activationToken,
+//   });
+// } 
+// )
+
+// //activation token used for time bound
+// export const verifyUser = TryCatch(async (req, res) => {
+//   const { otp, activationToken } = req.body;
+
+//   const verify = jwt.verify(activationToken, process.env.Activation_Secret);
+
+//   if (!verify) 
+//     return res.status(400).json({
+//       message: "Otp Expired",
+//     });
+// //verify.otp will have correct otp
+  
+//     if (verify.otp !== otp)
+//         return res.status(400).json({
+//           message: "Wrong Otp",
+//         });
+//         console.log(verify.user.name);
+//   await User.create({
+//     name: verify.user.name,
+//     email: verify.user.email,
+//     password: verify.user.password,
+//   });
+
+//   res.json({
+//     message: "User Registered Successfully",
+//   });
+// });
 
 
+// export const loginUser = TryCatch(async (req, res) => {
+//   const { email, password } = req.body;
+
+
+//   const user = await User.findOne({ email });
+
+//   if (!user)
+//     return res.status(400).json({
+//       message: "No User with this email",
+//     });
+
+//   const mathPassword = await bcrypt.compare(password, user.password);
+
+//   if (!mathPassword)
+//     return res.status(400).json({
+//       message: "wrong Password",
+//     });
+
+   
+
+//   const token = jwt.sign({ _id: user._id }, process.env.Activation_Secret, {
+//     expiresIn: "15d",
+//   });
+  
+
+//   res.json({
+//     message: `Welcome back ${user.name}`,
+//     token,
+//     user,
+//   });
+// });
+
+// export const myProfile = TryCatch(async (req, res) => {
+//   const user = await User.findById(req.user._id);
+
+//   res.json({ user });
+// });
+
+
+
+// import { User } from "../models/User.js";
+// import bcrypt from "bcrypt";
+// import jwt from "jsonwebtoken";
+// import sendMail from "../middlewares/sendMail.js";
+// import TryCatch from "../middlewares/TryCatch.js";
 
 export const register = TryCatch(async (req, res) => {
   const { email, name, password } = req.body;
@@ -53,32 +160,29 @@ export const register = TryCatch(async (req, res) => {
     otp,
   };
 
-  await sendMail(email, "next-edu", data);
+  await sendMail(email, "E learning", data);
 
   res.status(200).json({
     message: "Otp send to your mail",
     activationToken,
   });
-} 
-)
+});
 
-//activation token used for time bound
 export const verifyUser = TryCatch(async (req, res) => {
   const { otp, activationToken } = req.body;
 
   const verify = jwt.verify(activationToken, process.env.Activation_Secret);
 
-  if (!verify) 
+  if (!verify)
     return res.status(400).json({
       message: "Otp Expired",
     });
-//verify.otp will have correct otp
-  
-    if (verify.otp !== otp)
-        return res.status(400).json({
-          message: "Wrong Otp",
-        });
-        console.log(verify.user.name);
+
+  if (verify.otp !== otp)
+    return res.status(400).json({
+      message: "Wrong Otp",
+    });
+
   await User.create({
     name: verify.user.name,
     email: verify.user.email,
@@ -86,14 +190,12 @@ export const verifyUser = TryCatch(async (req, res) => {
   });
 
   res.json({
-    message: "User Registered Successfully",
+    message: "User Registered",
   });
 });
 
-
 export const loginUser = TryCatch(async (req, res) => {
   const { email, password } = req.body;
-
 
   const user = await User.findOne({ email });
 
@@ -109,12 +211,9 @@ export const loginUser = TryCatch(async (req, res) => {
       message: "wrong Password",
     });
 
-   
-
   const token = jwt.sign({ _id: user._id }, process.env.Activation_Secret, {
     expiresIn: "15d",
   });
-  
 
   res.json({
     message: `Welcome back ${user.name}`,
